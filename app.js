44db40569c08e3d442a2f60210aad04830b5e585
fryderykmuras <fryderyk.muras@gmail.com>
2019-06-08 12:07:13 +0200

    logging in

--- app.js
+++ app.js
@@ -245,6 +245,87 @@ app.post('/eatenMeals',(req,res)=>{
     res.send(validationResult.error.details[0].message);
     return
   }
+  
+})
+
+app.post('/register',(req,res)=>{
+  const newUser = req.body;
+  console.log(newUser);
+
+  connection.query(`SELECT * FROM Users WHERE LOGIN = '${newUser.USERNAME}'` , (err, rows, fields)=>{
+    if (err) {
+      console.log("Failed to query", err);
+      res.status(500);
+      res.send("Failed to query");
+      return
+    }else{
+      if(rows[0]){
+        console.log(rows);
+        res.status("409");
+        res.send("This username is taken");
+        return
+      }
+    }
+
+    connection.query(`SELECT * FROM Users WHERE EMAIL = '${newUser.EMAIL}'`,(err, rows, fields)=>{
+      if (err) {
+        console.log("Failed to query", err);
+        res.status(500);
+        res.send("Failed to query");
+        return
+      }else{
+        if(rows[0]){
+          console.log("email taken");
+          res.status("409");
+          res.send("This email is taken");
+          return
+        }
+        const values = [newUser.EMAIL, newUser.USERNAME, newUser.PASSWORD];
+        console.log(values);
+        connection.query(`INSERT INTO Users (EMAIL, LOGIN, PASSWORDHASH) VALUES (?)`,[values] , (err, rows, fields)=>{
+          if (err) {
+            console.log("Failed to query", err);
+            res.status(500);
+            res.send("Failed to query");
+            return
+          }else{
+            res.status(201).send();
+            return
+          }
+          
+        })
+        
+      }
+      
+    })
+    
+    
+  })
+
+})
+
+app.post('/login',(req,res)=>{
+  const user = req.body;
+
+  connection.query(`SELECT ID FROM Users WHERE EMAIL = '${user.EMAIL}' AND PASSWORDHASH = '${user.PASSWORD}'` , (err, rows, fields)=>{
+    if (err) {
+      console.log(err);
+      res.status(500)
+      res.end();
+      return
+    }
+    if(!rows[0]){
+      res.status(401).send();
+      return;
+    }else{
+      res.json({USERID: rows[0][0]})
+      res.end();
+    }
+    console.log(rows);
+    
+  })
+
+
 })
 
 app.listen(3000, ()=>{console.log('Listening...')})
